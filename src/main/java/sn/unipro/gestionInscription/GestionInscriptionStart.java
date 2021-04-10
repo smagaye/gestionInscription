8d07079b4ec535f3a144b19d7b7e468f95e435ef
Ada Gaye <sma.gaye94@gmail.com>
2021-04-10 11:38:24 +0000

    Délimitation du bloc de dépendance persistence et renseignement des champs de l'objet etudiant

--- src/main/java/sn/unipro/gestionInscription/GestionInscriptionStart.java
+++ src/main/java/sn/unipro/gestionInscription/GestionInscriptionStart.java
@@ -1,5 +1,6 @@
 package sn.unipro.gestionInscription;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import sn.unipro.gestionInscription.dao.EtudiantDao;
@@ -7,19 +8,27 @@ import sn.unipro.gestionInscription.modeles.Etudiant;
 
 public class GestionInscriptionStart {
 
-
- 
 	public static void main(String[] args) {
-        Etudiant etudiant = new Etudiant();
-        etudiant.setNom("Camara");
-        etudiant.setPrenom("Fallou");
-        etudiant.setMotPasse("1234");
-        etudiant.setDateNaissance(new Date());
-        
-        EtudiantDao etudiantDao = new EtudiantDao();
-        
-        etudiantDao.save(etudiant);
-        
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(1985, 04, 25);
+		Date dateNaissance = cal.getTime();
+		
+		EtudiantDao etudiantDao = new EtudiantDao();
+
+		Etudiant etudiant = new Etudiant();
+		etudiant.setNom("Camara");
+		etudiant.setPrenom("Fallou");
+		etudiant.setMotPasse("1234");
+		etudiant.setAdresse("Keur Massar");
+		etudiant.setCni("1870198504356");
+		etudiant.setEmail("fallou@gmail.com");
+		etudiant.setLieuNaissance("Dakar");
+		etudiant.setNumero_carte("UNI05298");
+		etudiant.setDateNaissance(dateNaissance);
+		
+		etudiantDao.save(etudiant);
+
 	}
 
 }
