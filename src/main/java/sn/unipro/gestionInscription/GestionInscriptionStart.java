929d0f03a29c68c5fb830590e8b33e1dd021c5e2
Ada Gaye <sma.gaye94@gmail.com>
2021-04-24 13:56:04 +0000

    Test sur le type Profile créer

--- src/main/java/sn/unipro/gestionInscription/GestionInscriptionStart.java
+++ src/main/java/sn/unipro/gestionInscription/GestionInscriptionStart.java
@@ -4,14 +4,16 @@ import sn.unipro.gestionInscription.dao.CompteDao;
 import sn.unipro.gestionInscription.dao.EtudiantDao;
 import sn.unipro.gestionInscription.modeles.Compte;
 import sn.unipro.gestionInscription.modeles.Etudiant;
+import sn.unipro.gestionInscription.modeles.Profil;
 import sn.unipro.gestionInscription.modeles.Sexe;
 import sn.unipro.gestionInscription.util.DateUtil;
 
 public class GestionInscriptionStart {
-
+	
+	private static EtudiantDao etudiantDao = new EtudiantDao();
+	private static CompteDao compteDao = new CompteDao();
+	
 	public static void main(String[] args) {
-
-		EtudiantDao etudiantDao = new EtudiantDao();		
 
 		Etudiant etudiant = new Etudiant();
 		etudiant.setNom("Camara");
@@ -25,21 +27,25 @@ public class GestionInscriptionStart {
 		etudiant.setSexe(Sexe.fromCode('m'));
 		etudiantDao.save(etudiant);
 		
-		CompteDao compteDao = new CompteDao();
+		String password = "1234";
+		
 		Compte compte = new Compte();
 		compte.setUsername("fallou.camara");
 		compte.setEmail("fallou.camara@unipro.sn");
-		compte.setPassword("1234");
+		compte.setPassword(password);
 		compte.setEtudiant(etudiantDao.findById(1));
+		compte.setProfil(Profil.fromCode("etu"));
 		compteDao.save(compte);
-		
+
 		compte = new Compte();
 		compte.setUsername("admin");
 		compte.setEmail("admin@unipro.sn");
-		compte.setPassword("1234");
+		compte.setPassword(password);
+		compte.setProfil(Profil.fromCode("adm"));
 		compteDao.save(compte);
-		
-		compteDao.login("fallou.camara", "1234");
+
+		compteDao.login("fallou.camara", password);	
+
 	}
 
 }
