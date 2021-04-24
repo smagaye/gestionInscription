aa49cd0d3db6586d5876da6ca58cada6523a3604
Ada Gaye <sma.gaye94@gmail.com>
2021-04-24 13:25:39 +0000

    Optimisation du JPAUtil

--- src/main/java/sn/unipro/gestionInscription/util/JPAUtil.java
+++ src/main/java/sn/unipro/gestionInscription/util/JPAUtil.java
@@ -5,14 +5,17 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-    private static final EntityManagerFactory emFactoryObj;
-    private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE_UNIT_DB"; 
-    
-    static {
-        emFactoryObj = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-    }
- 
-    public static EntityManager getEntityManager() {
-        return emFactoryObj.createEntityManager();
-    }
+	private static final EntityManagerFactory emFactoryObj;
+	private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE_UNIT_DB";
+	private static EntityManager entityManager;
+
+	static {
+		emFactoryObj = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+	}
+
+	public static EntityManager getEntityManager() {
+		if (entityManager == null)
+			entityManager = emFactoryObj.createEntityManager();
+		return entityManager;
+	}
 }
